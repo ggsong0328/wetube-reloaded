49f58d19a89a8b8006c7c66159d0d53638adddc2
ggsong0328 <ggsong0328@gmail.com>
2024-05-17 23:02:39 +0900

    request & response

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.")
-
+const handleHome = (req, res) => {
+    return res.send("<h1>I still love you.</h1>");
+};
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+};
 app.get("/", handleHome);
+app.get("/login", handleLogin)
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}🚀`);
 
