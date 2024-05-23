d0889f32b98be21a8665a59c1d2b5f37e9b26063
ggsong0328 <ggsong0328@gmail.com>
2024-05-23 11:41:00 +0900

    Controllers

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { watch, edit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit);
 
-videoRouter.get("/watch", handleWatchVideo);
 
 export default videoRouter;
